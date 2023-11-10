ab2835baf300f1119d908bf43e2963e46d321a22
Gian Neves <gianneves@hotmail.com>
2023-11-10 14:29:32 -0300

    feat: Create ParkingSpotController, ParkingSpotService-save

--- src/main/java/com/gianneves/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/gianneves/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.gianneves.parkingcontrol.services;
 
+import com.gianneves.parkingcontrol.models.ParkingSpotModel;
 import com.gianneves.parkingcontrol.respositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ public class ParkingSpotService {
 
     @Autowired
     ParkingSpotRepository parkingSpotRepository;
+
+    @Transactional
+    public Object save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
 }
